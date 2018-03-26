3996b1c2df68f4301439019d294c252c62246f4a
Joseph <JosephAQM@gmail.com>
2018-03-26 00:06:33 -0400

    Added basic audio reaction scripts - Created script that takes moving average of a audiosource's volume (dB) - Created a simple script that moves an object's height based on a audio source's dB )

---
+++ vaporworld/Assets/Scripts/AudioAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioAnalyzer : MonoBehaviour {
+
+    public AudioSource audioSource;
+    private float[] samples; // Wil hold our sample data
+    private float currDB;
+
+
+    void Start () {
+        samples = new float[1024]; // Allocate 1024 samples
+    }
+	
+	void Update () {
+        audioSource.GetOutputData(samples, 0);
+        currDB = convertToDB(samples);
+    }
+
+    private float convertToDB(float[] samples) {
+
+        float sumSquares = 0;
+
+        foreach (float sample in samples) {
+            sumSquares += sample * sample; //sample^2
+        }
+
+        // Calculate db -> 20*Log10(rms/ref)
+        // ref is just 1 here, so:
+        float db = 20 * Mathf.Log10(sumSquares);
+
+        return db;
+    }
+
+    public float getDB() {
+        return currDB;
+    }
+}
---
+++ vaporworld/Assets/Scripts/positionAudioReact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class positionAudioReact : MonoBehaviour {
+    public AudioAnalyzer analyzer; // Link analyzer script from parent object
+    public float minHeight;
+    public float maxHeight;
+    public float maxDB;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float currDB = analyzer.getDB();
+
+        if (currDB > maxDB) { currDB = maxDB; }
+
+        this.transform.position = new Vector3(this.transform.position.x, maxHeight * (currDB/ maxDB), this.transform.position.z);
+	}
+}
