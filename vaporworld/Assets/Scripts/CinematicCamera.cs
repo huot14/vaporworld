87d353ade5ee964ab1da9a90176b8991c541e79e
huot14 <colehuot14@gmail.com>
2018-04-03 15:47:11 -0400

    More camera controls

--- vaporworld/Assets/Scripts/CinematicCamera.cs
+++ vaporworld/Assets/Scripts/CinematicCamera.cs
@@ -13,12 +13,14 @@ public class CinematicCamera : MonoBehaviour {
     public float moveSpeed = 1.0f;
     public Vector3 pos1;
     public Vector3 pos2;
+    Vector3 currentPos;
 
     public float fovSpeed = 1.0f;
     public float fovMin = 1;
     public float fovMax = 170;
 
     bool increaseFov = true;
+    bool moveForward = true;
 
 
 	// Use this for initialization
@@ -26,7 +28,7 @@ public class CinematicCamera : MonoBehaviour {
     {
         cam = GetComponent<Camera>();
         fov = cam.fieldOfView;
-
+        currentPos = cam.transform.position;
 	}
 
     // Update is called once per frame
@@ -59,7 +61,31 @@ public class CinematicCamera : MonoBehaviour {
 
         if (moveMode)    // Change the position between pos1 and pos2
         {
-            //TODO
+            currentPos = cam.transform.position;
+
+            if (moveForward)
+            {
+               if(Vector3.Magnitude(currentPos - pos1) > 0f)
+                {
+                    cam.transform.position = Vector3.MoveTowards(cam.transform.position, pos1, moveSpeed * Time.deltaTime);
+                }
+               else
+                {
+                    moveForward = false;
+                }
+            }
+            else
+            {
+                if (Vector3.Magnitude(currentPos - pos2) > 0f)
+                {
+                    cam.transform.position = Vector3.MoveTowards(cam.transform.position, pos2, moveSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    moveForward = true;
+                }
+            }
+      
         }
 
 
---
+++ vaporworld/Assets/Scripts/SpinningCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningCamera : MonoBehaviour {
+
+    public float xSpeed = 1f;
+    public float ySpeed = 1f;
+    public float zSpeed = 1f;
+
+    Camera cam;
+    Vector3 rotation;
+
+    // Use this for initialization
+    void Start ()
+    {
+        cam = GetComponent<Camera>();
+        rotation = new Vector3(xSpeed, ySpeed, zSpeed);
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        cam.transform.Rotate(rotation * Time.deltaTime);
+	}
+}
