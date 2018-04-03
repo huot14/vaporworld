036b1e3c1b07f91d239f198d6e6420b038680053
huot14 <colehuot14@gmail.com>
2018-04-03 00:15:47 -0400

    The cat is alive

---
+++ vaporworld/Assets/Scripts/Cat.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour {
+
+    Transform cat;
+    public float speed = 3.0f;
+
+    public float distance = 50.0f;
+    float moved = 0.0f;
+
+    bool move = true;
+    bool forward = true;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cat = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (forward && moved < distance)
+        {
+            Vector3 move = cat.forward * Time.deltaTime * speed;
+            cat.Translate(move);
+            moved += Vector3.Magnitude(move);
+        }
+        else if(forward && moved >= distance)
+        {
+            forward = false;
+        }
+
+        if(!forward && moved > -distance)
+        {
+            Vector3 move = -cat.forward * Time.deltaTime * speed;
+            cat.Translate(move);
+            moved -= Vector3.Magnitude(move);
+        }
+        else if(!forward && moved <= -distance)
+        {
+            forward = true;
+        }
+	}
+}
---
+++ vaporworld/Assets/Scripts/CinematicCamera.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicCamera : MonoBehaviour {
+
+    Camera cam;
+    float fov;
+
+    public bool fovMode;
+    public bool moveMode;
+
+    public float moveSpeed = 1.0f;
+    public Vector3 pos1;
+    public Vector3 pos2;
+
+    public float fovSpeed = 1.0f;
+    public float fovMin = 1;
+    public float fovMax = 170;
+
+    bool increaseFov = true;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        cam = GetComponent<Camera>();
+        fov = cam.fieldOfView;
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fovMode)    // Change the field of view between fovMin and fovMax
+        { 
+
+            if (fov < fovMax && increaseFov)
+            {
+                fov += fovSpeed * Time.deltaTime;
+                cam.fieldOfView = fov;
+
+                if (fov >= fovMax)
+                {
+                    increaseFov = false;
+                }
+            }
+            else if (fov >= fovMin && !increaseFov)
+            {
+                fov -= fovSpeed * Time.deltaTime;
+                cam.fieldOfView = fov;
+
+                if (fov <= fovMin)
+                {
+                    increaseFov = true;
+                }
+            }
+        }
+
+        if (moveMode)    // Change the position between pos1 and pos2
+        {
+            //TODO
+        }
+
+
+    }
+}
