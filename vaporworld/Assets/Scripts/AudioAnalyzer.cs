fbb1d730009ec278da863ae9ba612adfcd160089
Joseph <JosephAQM@gmail.com>
2018-04-03 19:10:41 -0400

    Added audio react scripts that breaks sound down into frequency ranges - Also has moving average option to smooth movements

--- vaporworld/Assets/Scripts/AudioAnalyzer.cs
+++ vaporworld/Assets/Scripts/AudioAnalyzer.cs
@@ -7,15 +7,17 @@ public class AudioAnalyzer : MonoBehaviour {
     public AudioSource audioSource;
     private float[] samples; // Wil hold our sample data
     private float currDB;
-
+    private float[] currSpectrum;
 
     void Start () {
         samples = new float[1024]; // Allocate 1024 samples
+        currSpectrum = new float[64];
     }
 	
 	void Update () {
         audioSource.GetOutputData(samples, 0);
         currDB = convertToDB(samples);
+        currSpectrum = getSpectrum(audioSource);
     }
 
     private float convertToDB(float[] samples) {
@@ -33,7 +35,21 @@ public class AudioAnalyzer : MonoBehaviour {
         return db;
     }
 
+    private float[] getSpectrum(AudioSource source) {
+        float[] spectrum = new float[64];
+
+        source.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+
+        Debug.Log(spectrum[0]);
+
+        return spectrum;
+    }
+
     public float getDB() {
         return currDB;
+    }
+
+    public float[] getSpectrum() {
+        return currSpectrum;
     }
 }
--- vaporworld/Assets/Scripts/positionAudioReact.cs
+++ vaporworld/Assets/Scripts/positionAudioReact.cs
@@ -6,19 +6,58 @@ public class positionAudioReact : MonoBehaviour {
     public AudioAnalyzer analyzer; // Link analyzer script from parent object
     public float minHeight;
     public float maxHeight;
-    public float maxDB;
+    public float maxLevel; // Max total sum of the spectrum range
+
+    public int rangeStart;
+    public int rangeEnd;
+
+    public Queue<float> movingAverage; // Each index corresponds to the rangeTotal at a period of time
+    public int movingAverageWidth;
 
 	// Use this for initialization
 	void Start () {
-		
+        maxLevel = 10;
+        movingAverage = new Queue<float>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float currDB = analyzer.getDB();
+        float[] currSpectrum = analyzer.getSpectrum();
 
-        if (currDB > maxDB) { currDB = maxDB; }
+        float rangeTotal = 0;
 
-        this.transform.position = new Vector3(this.transform.position.x, maxHeight * (currDB/ maxDB), this.transform.position.z);
+        // Sum total of values in desired range
+        for (int i  = rangeStart; i < rangeEnd; i++) {
+            rangeTotal += currSpectrum[i];
+        }
+
+        updateMovingAverage(rangeTotal);
+
+        float averageOfTotals = sumQueue(movingAverage);
+
+        if (averageOfTotals > maxLevel) { averageOfTotals = maxLevel; }
+
+        this.transform.position = new Vector3(this.transform.position.x, maxHeight * (averageOfTotals / maxLevel), this.transform.position.z);
 	}
+
+    void updateMovingAverage(float newValue) {
+        movingAverage.Enqueue(newValue);
+        
+        if (movingAverage.Count > movingAverageWidth){
+            movingAverage.Dequeue();
+        }
+    }
+
+    // Sums all values in a queue
+    float sumQueue(Queue<float> queue) {
+        float sum = 0;
+
+        float[] queueValues = queue.ToArray();
+
+        foreach (float value in queueValues) {
+            sum += value;
+        }
+
+        return sum;
+    }
 }
